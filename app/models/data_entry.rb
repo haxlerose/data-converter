e61404bf0fbf9c3e90987a012f500e8b9483e237
paul.chesterton <paul.chesterton@adlumin.com>
2024-06-20 14:31:16 -0400

    update model relationships

--- app/models/data_entry.rb
+++ app/models/data_entry.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class DataEntry < ApplicationRecord
-  belongs_to :data_type
+  belongs_to :data_type, touch: true
+  has_one_attached :file, optional: true, dependent: :destroy
+  validates :data_content, presence: true
 end
--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class DataType < ApplicationRecord
-  has_many :data_entries
+  has_many :data_entries, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
